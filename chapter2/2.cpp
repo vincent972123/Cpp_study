4135c9d44d3b8a43c96ea906eb7d3f27cb762869
vincent972123 <392025574@qq.com>
2021-07-18 09:48:47 +0800

    1

--- chapter2/2.cpp
+++ chapter2/2.cpp
@@ -86,13 +86,85 @@ int main()
 }
 */
 
+// test code
+/* #include<iostream>
+#include<typeinfo>
+int main()
+{
+    int i = 0, &r = i;
+    auto a = r; // int
+    const int ci = i, &cr = ci;
+    auto b = ci; // int
+    auto c = cr; // const int
+    auto d = &i; // Pi
+    auto e = &ci; // Pki
+    const auto f = ci; // const int 
+    auto &g = ci; // int const reference
+    //auto &h = 42; 
+    const auto &j = 42;
+    auto k = ci, &l = i; //int, int reference
+    
+    //const int &m = ci, *p = &ci;
+    auto &m = ci, *p = &ci; // int const reference, point int const pointer
+    
+    //auto &n = i, *p2 = &ci; 
+    //std::cout << typeid(c).name() << std::endl;
+    //std::cout << typeid(d).name() << std::endl;
+    //std::cout << typeid(m).name() << std::endl;
+    //std::cout << typeid(p).name() << std::endl;
+    //std::cout << p <<std::endl;
+    //std::cout << *p <<std::endl; 
+
+    a = 42;
+    b = 42;
+    c = 42;
+    d = 42;
+    e = 42;
+    f = 42;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    std::cout << e << std::endl;
+    std::cout << f << std::endl;
+    return 0;
+
+}
+*/
+
+
+
+// prictice 2.35
+/* 
+#include<iostream>
+#include<typeinfo>
+int main()
+{
+    const int i = 42; // const int 
+    auto j = i; // int
+    const auto &k = i; // const int
+    auto *p = &i; // pointer to const int
+    const auto j2 = i, &k2 = i; // int
+    std::cout << typeid(i).name() << std::endl;
+    std::cout << typeid(j).name() << std::endl;
+    std::cout << typeid(k).name() << std::endl;
+    std::cout << typeid(p).name() << std::endl;
+    std::cout << typeid(j2).name() << std::endl;
+    std::cout << typeid(k2).name() << std::endl;
+    return 0;
+} 
+*/
+
+// decltype
+#include<typeinfo>
 #include<iostream>
 int main()
 {
-    int i = 5;
-    int &r1 = i;
-    const int &r2 = i;
-    r1 = 10;
-    std::cout << r2 << std::endl;
+    const int ci = 0, &cj = ci;
+    decltype(ci) x = 0;
+    decltype(cj) y = x;
+    std::cout << typeid(x).name() << std::endl;
+    std::cout << y << std::endl;
+
     return 0;
 }
