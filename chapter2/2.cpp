b60f74886aaee587090443d8042fc1aebd3f4664
vincent972123 <392025574@qq.com>
2021-07-19 16:07:29 +0800

    1

--- chapter2/2.cpp
+++ chapter2/2.cpp
@@ -156,6 +156,7 @@ int main()
 */
 
 // decltype
+/* 
 #include<typeinfo>
 #include<iostream>
 int main()
@@ -164,7 +165,86 @@ int main()
     decltype(ci) x = 0;
     decltype(cj) y = x;
     std::cout << typeid(x).name() << std::endl;
+    std::cout << typeid(y).name() << std::endl;
+    
     std::cout << y << std::endl;
 
     return 0;
+} 
+*/
+
+// prictice 2.38
+/* 
+#include<iostream>
+#include<typeinfo>
+int main()
+{
+    int a = 3;
+    auto c1 = a;
+    decltype(a) c2 = a;
+    decltype((a)) c3 = a;
+    
+    const int d = 5;
+    auto f1 =  d; // auto 忽略顶层const
+    decltype(d) f2 = d; // decltype 保留顶层const
+
+    std::cout << typeid(c1).name() << std::endl;
+    std::cout << typeid(c2).name() << std::endl;
+    std::cout << typeid(c3).name() << std::endl;
+    std::cout << typeid(f1).name() << std::endl;
+    std::cout << typeid(f2).name() << std::endl;
+
+    c1++;
+    c2++;
+    c3++;
+    f1++;
+    //f2++;
+
+    std::cout << c1 << " " << c2 << " " << c3 << " " << f1 << " " << f2 << " " << std::endl;
+    return 0;
+
 }
+*/
+ 
+// 2.6.2 class Sales_data
+
+#include<iostream>
+#include<string>
+#include"Sales_data.h"
+/*
+struct Sales_data
+{
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+*/
+int main()
+{
+    Sales_data data1, data2;
+    double price = 0.0;
+    std::cin >> data1.bookNo >> data1.units_sold >> price;
+    data1.revenue = data1.units_sold * price;
+    std::cin >> data2.bookNo >> data2.units_sold >> price;
+    data2.revenue = data2.units_sold * price;
+    if(data1.bookNo == data2.bookNo){
+        unsigned units_sold_sum = data1.units_sold + data2.units_sold;
+        double revenue_sum = data1.revenue + data2.revenue;
+        std::cout << data1.bookNo << " " << units_sold_sum << " " << revenue_sum << " "; 
+        if(units_sold_sum != 0){
+            std::cout << revenue_sum/units_sold_sum << std::endl;
+        }
+        else{
+            std::cout << "no sales" << std::endl;
+        }
+    }
+    else{
+        std::cout << "DATA must refer to the same ISBN" << std::endl;
+        return -1;
+    }
+    return 0;
+}
+
+
+
+
